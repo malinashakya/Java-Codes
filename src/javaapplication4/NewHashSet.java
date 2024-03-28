b904bd6b6e14682f624b76eab33298228c5b7463
Malina <malinashakya72@gmail.com>
2024-03-28 20:22:16 +0545

    Logic changed in NewHashSet

--- src/javaapplication4/NewHashSet.java
+++ src/javaapplication4/NewHashSet.java
@@ -7,11 +7,12 @@ public class NewHashSet {
     public Object[] array;
     public int size;
     private int capacity = 5;
+    private Iterator<Object> innerIterator; // Iterator for inner sets
+    private boolean iteratorCreated = false; // Flag to track iterator creation
 
     public NewHashSet() {
         array = new Object[capacity];
         size = 0;
-
     }
 
     public NewHashSet(int capacity) {
@@ -21,13 +22,13 @@ public class NewHashSet {
     }
 
     public boolean add(Object value) {
-        //same old value
+        // Same old value
         if (contains(value)) {
             return false;
         }
         int index = getIndex(value);
 
-        //If old value and new value is different
+        // If old value and new value are different
         if (array[index] != null) {
             NewHashSet collisionset = new NewHashSet(capacity * 2);
             Object oldValue = array[index];
@@ -42,7 +43,7 @@ public class NewHashSet {
     }
 
     public boolean remove(Object value) {
-        //there is no such value
+        // There is no such value
         if (!contains(value)) {
             return false;
         }
@@ -68,64 +69,82 @@ public class NewHashSet {
 
     public int getIndex(Object obj) {
         int index = Math.abs(obj.hashCode() % capacity);
-
         return index;
     }
 
     public boolean contains(Object value) {
         int index = getIndex(value);
         Object previousValue = array[index];
-        //Inside CollisionHandleSet
+        // Inside CollisionHandleSet
         if (previousValue instanceof NewHashSet) {
             NewHashSet collisionHandleSet = (NewHashSet) previousValue;
             return collisionHandleSet.contains(value);
-        } //If there is no CollisionHandleSet
-        else {
+        } else {
+            // If there is no CollisionHandleSet
             return (previousValue != null && previousValue.equals(value));
+        }
+    }
+
+    class IteratorImpl implements Iterator {
+
+        private int currentIndex = 0;
+       private Iterator<Object> storedIterator = null;
+
+        /**
+         * If array[currentIndex] is of NewHashSet type, we should iterate
+         * inside the NewHashSet that is made inside the array[currentIndex].
+         * And after reading the elements inside the NewHashSet type which is
+         * made inside the array[currentIndex] we should again move back to the
+         * previous array with the index+1 and iterate in the old NewHashSet
+         */
+        @Override
+        public boolean hasNext() {
+            if (array[currentIndex] instanceof NewHashSet) {
+                /**
+                 * Take iterator from HashSet and store it to use the same iterator later
+                 */
+                  if (storedIterator == null) {
+                NewHashSet innerHashSet = ((NewHashSet) array[currentIndex]);
+                storedIterator = innerHashSet.iterator();
+            }
+               
+                return storedIterator.hasNext();
+            }
+            while (currentIndex < capacity) {
+                if (array[currentIndex] != null) {
+                    return true;
+                }
+                currentIndex++;
+            }
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            return array[currentIndex++];
         }
 
     }
 
     public Iterator<Object> iterator() {
-        return new Iterator<Object>() {
-            private int currentIndex = 0;
-
-            @Override
-            public boolean hasNext() {
-                return currentIndex < capacity;
-            }
-
-            @Override
-            public Object next() {
-                return array[currentIndex++];
-            }
-        };
+        //defining method inside Iterator
+        return new IteratorImpl();
     }
-// Method to print all elements in the Outer class i.e. NewHashSet
-public void printAllElements() {
-    Iterator<Object> iterator = iterator();
-    while (iterator.hasNext()) {
-        Object element = iterator.next();
 
-        if (element instanceof NewHashSet) {
-            NewHashSet collisionHandleSet = (NewHashSet) element;
-            collisionHandleSet.printAllElements();
-        } else if (element != null) {
-            System.out.println("Element: " + element);
+    public void printAllElements() {
+        Iterator<Object> iterator = iterator();
+        while (iterator.hasNext()) {
+            Object element = iterator.next();
+            if (element instanceof NewHashSet) {
+                NewHashSet collisionHandleSet = (NewHashSet) element;
+                collisionHandleSet.printAllElements();
+            } else {
+                System.out.println("Element: " + element);
+            }
         }
     }
-}
-
-    //Delegate method
-
-//    public void printAllElements() {
-//        printAllElements("");
-//
-//    }
 
     public static void main(String[] args) {
-//        Object ob = "abc";
-//        System.out.println(ob instanceof String);
         NewHashSet set = new NewHashSet();
 
         set.add("Hello");
@@ -141,5 +160,4 @@ public void printAllElements() {
         System.out.println("After removing 123:");
         set.printAllElements();
     }
-
 }
