7b1e0d4a8c61199882b90b663b0051c1b4347192
Malina <malinashakya72@gmail.com>
2024-04-22 21:01:33 +0545

    Added isSorted

--- src/javaapplication4/IsArraySorted.java
+++ src/javaapplication4/IsArraySorted.java
@@ -11,35 +11,22 @@ package javaapplication4;
 public class IsArraySorted {
 
     public static boolean isSorted(int[] array) {
-        int temp;
-        Boolean mark = false;
-        int[] array_temp = new int[array.length];
-        array_temp = array;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                if (array[i] > array[i]) {
-                    temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
-                }
-            }
+    for(int i=0;i<array.length-1;i++)
+    {
+        if(array[i]>array[i+1])
+        {
+            return false;
         }
-        for (int i = 0; i < array_temp.length; i++) {
-            if (array[i] == array_temp[i]) {
-                mark = true;
-            }
-            else{
-            mark = false;
-            }
-        }
-        return mark;
+        
+    }
+     return true;
     }
 
     public static void main(String[] args) {
         int[] array1 = {1, 3, 5, 7};
         int[] array2 = {9, 4, 3};
-        System.out.println("Array1"+isSorted(array1)); 
-        System.out.println("Array2"+isSorted(array2));
+        System.out.println("Array1 is sorted?"+isSorted(array1)); 
+        System.out.println("Array2 is sorted?"+isSorted(array2));
 
     }
 }
