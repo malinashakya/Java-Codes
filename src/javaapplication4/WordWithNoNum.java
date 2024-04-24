5f7dcb7a68b00bf04d4466c51a942d67dd2a10e9
Malina <malinashakya72@gmail.com>
2024-04-24 15:19:26 +0545

    Added Word with no number

---
+++ src/javaapplication4/WordWithNoNum.java
@@ -0,0 +1,32 @@
+    /*
+     * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+     * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+     */
+    package javaapplication4;
+
+    /**
+     *
+     * @author malin
+     */
+    public class WordWithNoNum {
+        public static void main(String[] args) {
+            String paragraph=" This is3 the program that prints word with no numbers33";
+            String[] words=paragraph.split("\\s+");
+            for(int i=0;i<words.length;i++)
+            {
+                boolean Digit=false;
+                for(int j=0;j<words[i].length();j++)
+                {
+                    if(Character.isDigit(words[i].charAt(j)))
+                    {
+                        Digit=true;
+                       break;
+                    }
+
+                }
+                if(!Digit)
+                 System.out.println(words[i]);
+            }
+        }
+
+    }
